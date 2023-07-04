ada8a829cec26787c741c3b74f3f5c0226feaaa2
Flávio Medeiros <flaviomotamedeiros@gmail.com>
2023-07-04 20:29:16 -0300

    Aplicativo mínimo estruturado com o Scaffold.

--- minimal/lib/main.dart
+++ minimal/lib/main.dart
@@ -9,8 +9,17 @@ class Minimal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('Minimal App'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Minimal App'),
+        ),
+      ),
     );
   }
 }
+
+// Scaffold é uma classe em flutter que fornece muitos widgets,
+// tais como: Drawer, Snack-Bar, Bottom-Navigation-Bar, Floating-Action-Button, 
+// App-Bar, entre outros. Scaffold irá expandir ou ocupar toda a tela do 
+// dispositivo. Ele ocupará o espaço disponível.
